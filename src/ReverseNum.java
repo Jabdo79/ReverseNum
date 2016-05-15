ca2b64604c25482e3afd4cd19843291514f0f2e4
Jonathan Abdo <jabdo79@gmail.com>
2016-05-15 19:39:05 -0400

    Create empty class and add comment header

---
+++ src/ReverseNum.java
@@ -0,0 +1,12 @@
+/*Jonathan Abdo
+This class will ask the user for a number, and 
+then print the reverse to the console.
+*/
+
+public class ReverseNum {
+	
+	public static void main(String[] args) {
+
+	}
+
+}
