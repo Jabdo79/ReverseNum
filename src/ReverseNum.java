c91007588271d67d2737af925e0b72f09c1f91c1
Jonathan Abdo <jabdo79@gmail.com>
2016-05-16 14:45:28 -0400

    Simplify input before merge with master branch

--- src/ReverseNum.java
+++ src/ReverseNum.java
@@ -6,20 +6,15 @@ then print the reverse to the console.
 import java.util.Scanner;
 
 public class ReverseNum {
-	
+
 	static Scanner input = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
-		double num = 0;
-		
+
+		int num = 0;
+
 		System.out.print("Please enter a number: ");
-		if (input.hasNextDouble()) {
-			num = input.nextDouble();
-		}
-		else {
-			System.out.println("That wasn't a number, please try again.");
-		}
-		
+		num = input.nextInt();
+		System.out.println("Your number is "+num);
 	}
 }
