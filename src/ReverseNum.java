423b2de58be674e3fd9fc24e3293415f44ad1d89
Jonathan Abdo <jabdo79@gmail.com>
2016-05-15 20:00:34 -0400

    Add input stream and ask user to enter number

--- src/ReverseNum.java
+++ src/ReverseNum.java
@@ -3,9 +3,24 @@ This class will ask the user for a number, and
 then print the reverse to the console.
 */
 
+import java.util.Scanner;
+
 public class ReverseNum {
 	
+	static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
+		
+		int num = 0;
+		
+		try {
+			System.out.print("Please enter a number: ");
+			num = input.nextInt();
+			System.out.println("Your number is " + num);
+		} catch (Exception e) {
+			System.out.println("That wasn't a number, please try again.");
+		}
+		
 
 	}
 
